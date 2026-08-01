f19b09c463a01837fb3c23ab8d11c53628385bca
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-08-01 16:51:33 +0530

    Add menu of calculator.

---
+++ Electrical_calculator/src/main.m
@@ -0,0 +1,54 @@
+while true 
+clc;
+fprintf("=================================== \n");
+fprintf("     ELECTRICAL CALCULATOR v1.0   \n");
+fprintf("=================================== \n");
+fprintf("\n")
+fprintf("1. Voltage \n");
+fprintf("2. Current \n");
+fprintf("3. Resistance \n");
+fprintf("4. Power \n");
+fprintf("5. Energy \n");
+fprintf("0. Exit \n");
+fprintf("\n");
+fprintf("-----------------------------------\n");
+choice = input("Enter your choice: ");
+switch choice
+
+    case 1
+        disp("Voltage Calculator");
+        fprintf("\n");
+        pause;
+
+    case 2
+        disp("Current Calculator");
+        fprintf("\n");
+        pause;
+
+    case 3
+        disp("Resistance Calculator");
+        fprintf("\n");
+        pause;
+
+    case 4
+        disp("Power Calculator");
+        fprintf("\n");
+        pause;
+
+    case 5
+        disp("Energy Calculator");
+        fprintf("\n");
+        pause;
+
+    case 0
+        disp("Thank you for using Electrical Calculator.");
+        fprintf("\n");
+        break;
+
+    otherwise 
+        disp("Invalid Choice!");
+        fprintf("\n");
+        pause;
+
+end
+end
