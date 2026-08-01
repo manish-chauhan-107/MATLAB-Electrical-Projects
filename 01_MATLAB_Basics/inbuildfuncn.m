6cd3df0802fa963be2eac3874bcdb79c2f3fa141
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-08-01 21:51:34 +0530

    Add some in build function information .

--- 01_MATLAB_Basics/inbuildfuncn.m
+++ 01_MATLAB_Basics/inbuildfuncn.m
@@ -3,6 +3,8 @@
 y = linspace(1,10,5);
 disp(y);
 % use of "rand()" function 
-% create a square Metrix with random numbers 
+% create a Metrix with random numbers 
 x = rand(5);
 disp(x);
+%for find the size of a metrix
+disp(size(x));
