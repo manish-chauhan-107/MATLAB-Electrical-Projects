143c667329f4b3e2df93eb943ce687e05227c054
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-08-02 09:51:17 +0530

    Add basic of annotate plot .

---
+++ 01_MATLAB_Basics/annotate_plot.m
@@ -0,0 +1,6 @@
+%You can add labels to plots using plot annotation functions, such as title. The input to these functions is text. Text in MATLAB is enclosed in double quotes (").
+title("Plot Title")
+%You can add labels to your axes using the xlabel and ylabel functions.
+ylabel("Y-Axis Label")
+%You can add a legend to your plot using the legend function.
+legend("a","b","c")
