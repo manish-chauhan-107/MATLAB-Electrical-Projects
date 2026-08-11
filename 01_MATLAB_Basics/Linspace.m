d9cf45c1fdb1ad62c4876457b12a0a2e4413dbd3
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-08-11 22:53:43 +0530

    update

--- 01_MATLAB_Basics/Linspace.m
+++ 01_MATLAB_Basics/Linspace.m
@@ -1,4 +1,3 @@
-%linspace(first , last , number of elements) used to get (number of random
-%elements in provided range)
+%linspace(first , last , number of elements) used to make saperation.=
 x = linspace(1,10,5);
 disp(x);
