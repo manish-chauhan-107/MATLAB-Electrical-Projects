27eb2dc0e14a991aed4b620569c47a3039fade07
Manish Chauhan <nitm.manishchauhan@gmail.com>
2026-08-11 23:08:50 +0530

    update

--- 01.1_MATLAB_Adv/src/generateSine.m
+++
@@ -1,6 +0,0 @@
-function [t, signal] = generateSine(amplitude, frequency, duration, samplingFrequency)
-
-t = linspace(0, duration, duration * samplingFrequency);
-signal = amplitude * sin(2 * pi * frequency * t);
-
-end
--- 01.1_MATLAB_Adv/src/generatesquare.m
+++
@@ -1,5 +0,0 @@
-% generatesquare.m
-Fs = 1000;                    % sampling frequency (Hz)
-t = linspace(0, 2*pi, 3);    % time vector from 0 to 2*pi
-x = square(t);                % default 50% duty-cycle square wave
-
